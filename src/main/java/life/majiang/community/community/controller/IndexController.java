dee4d506f7bb2211b5500081fec4dcc0258dc361
unknown <1553312887@qq.com>
2019-10-03 23:59:06 +0800

    bootstrap

---
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -0,0 +1,16 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @program: community
+ * @description:
+ * @author: LiangZai
+ * @create: 2019-10-03 23:53
+ **/
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){ return "index";}
+}
