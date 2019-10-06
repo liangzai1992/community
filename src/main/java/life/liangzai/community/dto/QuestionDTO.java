d6c393c590c51c276cf0bbf700d78ebecdf14b23
unknown <1553312887@qq.com>
2019-10-06 12:20:01 +0800

    首页列表

---
+++ src/main/java/life/liangzai/community/dto/QuestionDTO.java
@@ -0,0 +1,10 @@
+package life.liangzai.community.dto;
+
+/**
+ * @program: community
+ * @description:
+ * @author: LiangZai
+ * @create: 2019-10-06 07:54
+ **/
+public class QuestionDTO {
+}
---
+++ src/main/java/life/liangzai/community/service/QuestionService.java
@@ -0,0 +1,10 @@
+package life.liangzai.community.service;
+
+/**
+ * @program: community
+ * @description:
+ * @author: LiangZai
+ * @create: 2019-10-06 07:57
+ **/
+public class QuestionService {
+}
